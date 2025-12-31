db93e1654e3d318d1311232bd5da962adf10cc97
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2025-12-31 13:32:04 +0530

    Update schemas.py

--- api/schemas.py
+++ api/schemas.py
@@ -3,4 +3,6 @@ from pydantic import BaseModel
 class Transaction(BaseModel):
     user_id: int
     amount: float
+    merchant: str | None = None
+    country: str | None = None
     timestamp: str
