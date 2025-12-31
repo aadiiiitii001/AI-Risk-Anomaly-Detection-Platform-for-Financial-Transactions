046fb5f7cbf6cb9b98953d0ab8d58273d75ad0a0
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2025-12-31 11:57:32 +0530

    Create main.py

---
+++ api/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from schemas import Transaction
+from predict import predict_transaction
+
+app = FastAPI(title="AI Risk & Anomaly Detection Platform")
+
+@app.post("/predict")
+def predict(tx: Transaction):
+    return predict_transaction(tx.dict())
