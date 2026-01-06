c3cab3e54bda72423f9b3f66b8e38ab37377abd1
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2026-01-06 14:10:01 +0530

    Update database.py

--- db/database.py
+++ db/database.py
@@ -1,14 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://user:password@localhost:5432/finrisk"
-)
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./finance_ai.db")
 
 engine = create_engine(
     DATABASE_URL,
+    connect_args={"check_same_thread": False}
+    if DATABASE_URL.startswith("sqlite")
+    else {},
     pool_pre_ping=True
 )
 
