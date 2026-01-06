80daab0fd6a83d78969bddf40af744523dd7781c
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2026-01-06 14:11:56 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 from api.schemas import Transaction
 from api.predict import predict_transaction
+from db.database import engine
+from db.models import Base
 
 app = FastAPI(title="AI Risk & Anomaly Detection Platform")
+
+# ✅ Ensure SQLite tables are created on startup
+@app.on_event("startup")
+def startup_event():
+    Base.metadata.create_all(bind=engine)
 
 @app.post("/predict")
 def predict(tx: Transaction):
